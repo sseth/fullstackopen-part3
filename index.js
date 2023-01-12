875ae2bc2cd6646366735f9929e8f1ef8ffd31ff
Shubhankar Seth <shubhseth@gmail.com>
2023-01-13 02:02:20 +0530

    Part 3a, ex 3.1 - 3.8: phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(express.json());
+morgan.token('request-data', (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens['request-data'](req, res),
+    ].join(' ');
+  })
+);
 
-const data = [
+let data = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -29,10 +46,6 @@ app.get('/api', (req, res) => {
   res.send('Hello');
 });
 
-app.get('/api/persons', (req, res) => {
-  res.send(data);
-});
-
 app.get('/api/info', (req, res) => {
   const info = `Phonebook has info for ${data.length} ${
     data.length === 1 ? 'person' : 'people'
@@ -41,6 +54,44 @@ app.get('/api/info', (req, res) => {
     `<p>${info}</p>
       <p>${Date()}</p>`
   );
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(data);
+});
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body;
+  if (!name || !number)
+    return res.status(400).json({
+      error: 'name and number required',
+    });
+
+  if (data.find((p) => p.name.toLowerCase() === name.toLowerCase()))
+    return res.status(400).json({
+      error: `'${name}' already exists`,
+    });
+
+  const id = Math.ceil(Math.random() * 10000);
+  const newEntry = { id, name, number };
+  data.push(newEntry);
+  res.status(201).json(newEntry);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params;
+  const entry = data.find((p) => p.id === Number(id));
+  if (!entry)
+    return res.status(404).json({
+      error: `phonebook entry with id ${id} not found`,
+    });
+  res.json(entry);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  data = data.filter((p) => p.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
